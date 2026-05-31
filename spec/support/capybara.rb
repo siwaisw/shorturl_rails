ef8d109815f02c53bf712ffeb6c7ac85df3214f8
siwai <siwaisoftware@gmail.com>
2026-05-31 17:16:19 +0800

    rubocop fix

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,11 +5,9 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
-
-  # JS tests use headless Chrome. These require DatabaseCleaner because Selenium
-  # runs in a separate thread and cannot see data wrapped in a test transaction.
+  # Use Selenium with headless Chrome for JS-enabled tests (slower, requires browser process, cannot use transactional fixtures).
   # See spec/support/database_cleaner.rb (add when you need js: true specs).
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 900]
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 900 ]
   end
 end
